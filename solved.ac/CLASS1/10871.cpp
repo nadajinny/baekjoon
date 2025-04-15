867b970667c1212cf2fac269d6d09c89bc836488
nadajinny <jinsun23007@gmail.com>
2025-04-15 23:18:26 +0900

    solve

--- solved.ac/CLASS1/10871.cpp
+++ solved.ac/CLASS1/10871.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 int main(void){
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    
     vector<int> a; 
     int n, x; 
     int sample;
+
     cin>>n>>x;
+
     for(int i=0;i<n;i++){
         cin>> sample;
         a.push_back(sample);
